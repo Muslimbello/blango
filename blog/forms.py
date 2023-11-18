f4d7545ed38b45d47094e34f4bbe29bd2ca17f2d
Muslim Bello <muslimbello123@gmail.com>
2023-11-18 09:10:50 +0000

    Completed crispy forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
 from blog.models import Comment
 
 class CommentForm (forms.ModelForm):
-  model = Comment
-  fields =['content']
+  class Meta :
+    model = Comment
+    fields = ["content"]
+  # print(type(Comment))
+  def __init__(self, *args , **kwargs):
+    super(CommentForm, self).__init__(*args ,**kwargs )
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
