24b8a27e1fa1ad4ba97fc2a42a3dfc10400a994d
Muslim Bello <muslimbello123@gmail.com>
2023-11-19 17:30:54 +0000

    Completed cashing

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render ,get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 from blog.forms import CommentForm
 from blog.models import  Post
+from django.http import HttpResponse
 # Create your views here.
 import logging
 
 logger = logging.getLogger(__name__)
-def index(request):
-  posts = Post.objects.filter(published_at__lte = timezone.now())
+@cache_page(300)
+@vary_on_cookie
+def index(request): 
+  posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
-  return render(
-    request, "blog/index.html", {"posts": posts}
-    )
-  
+  return render(request, "blog/index.html", {"posts": posts})
+
 def post_detail(request, slug):
   post = get_object_or_404 (Post, slug= slug)
   if request.user.is_active:
