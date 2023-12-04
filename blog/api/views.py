e527bcc01915bc3276f1f1fd050a474e9689a25f
Muslim Bello <muslimbello123@gmail.com>
2023-12-04 23:34:37 +0000

    Completed Related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -6,7 +6,8 @@
 from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
-
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -14,3 +15,8 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+  lookup_field = "email"
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
